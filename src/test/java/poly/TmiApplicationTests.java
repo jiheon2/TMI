1560e8fa4d7e0058fc0857ffd74801e019e1687f
Kim Ji Heon <000224kjh@gmail.com>
2023-09-11 17:57:23 +0900

    생성 파일

---
+++ src/test/java/poly/TmiApplicationTests.java
@@ -0,0 +1,13 @@
+package poly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
