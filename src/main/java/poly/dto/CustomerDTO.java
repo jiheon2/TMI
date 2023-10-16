8e2fa5b02266e4cc8b2a02d81bc24ea51c26b8dc
jihoen2 <kch3453@naver.com>
2023-10-16 16:44:13 +0900

    DTO제작 및 HTML 수정

---
+++ src/main/java/poly/dto/CustomerDTO.java
@@ -0,0 +1,14 @@
+package poly.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CustomerDTO {
+    private String id;
+    private String pw;
+    private String pn;
+    private String name;
+    private String socialNumber;
+}
